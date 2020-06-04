ea41ca90607b0f72f70c908f7b43d11e2fe3b7e4
Sydney <s1811552@s.tsukuba.ac.jp>
2020-06-04 15:26:48 +0900

    edit profile.py

--- cgi-bin/profile.py
+++ cgi-bin/profile.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
 #encoding:UTF-8
-def make_user_data():
+def display_user_data():
     str = ""
     str += textwrap.dedent('''
-        <p>
-          <input type="radio" name="gender" value="men" checked="checked">Men
-          <input type="radio" name="gender" value="women">Women
-        </p>
-        住所<input type="text" name="zipcode" size="40" maxlength="8">
+
     ''')
 
     return str
@@ -38,12 +34,8 @@ html = textwrap.dedent('''
 <title>Dresser</title>
 </head>
     <body>
-      <h1>input your data</h1>
-      <form action="./done.py" method="post">
-      {0}
-      <input type="submit" value="Submit!">
-      </form>
+      
     </body>
 </html>
-''').format(make_user_data()).strip()
+''').format(display_user_data()).strip()
 print(html)
