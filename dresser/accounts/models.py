cc3d5e85a7c14ee6f811ad7fab3720e9d435ce14
Sydney <s1811552@s.tsukuba.ac.jp>
2020-06-04 15:51:06 +0900

    delete django app

--- dresser/accounts/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-#from django.contrib.auth.models import AbstractUser
-# Create your models here.
-
-#class CustomUser(AbstractUser):
-#    """拡張ユーザーモデル"""
-
-#    class Meta:
-#        verbose_name_plural = "CustomUser"
--- dresser/recommender/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views import View
-from django.views.generic import CreateView, TemplateView
-# Create your views here.
-
-
-class Recommender(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'recommender.html')
